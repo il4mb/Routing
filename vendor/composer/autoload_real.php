6d522f4b1c8e6d1ab9a53d456629ce3beb9d3078
ilham b <durianbohong@gmail.com>
2025-01-14 00:59:34 +0700

    Changes to be committed: 	new file:   composer.lock 	new file:   vendor/composer/InstalledVersions.php 	modified:   vendor/composer/autoload_real.php 	new file:   vendor/composer/installed.json 	new file:   vendor/composer/installed.php 	new file:   vendor/composer/platform_check.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1354e2fbcb68545ab943cd6cb921cd5b
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1354e2fbcb68545ab943cd6cb921cd5b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit1354e2fbcb68545ab943cd6cb921cd5b', 'loadClassLoader'));
